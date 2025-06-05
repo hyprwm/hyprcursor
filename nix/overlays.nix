aa3feb04008eaf8ea58549e06c10b357b0128c9e
Friday <faeriethe@gmail.com>
2025-06-05 20:22:12 +0100

    nix: use gcc15
    also updated dependencies

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -15,7 +15,7 @@ in {
     inputs.hyprlang.overlays.default
     (final: prev: {
       hyprcursor = prev.callPackage ./default.nix {
-        stdenv = prev.gcc14Stdenv;
+        stdenv = prev.gcc15Stdenv;
         version = version + "+date=" + (mkDate (inputs.self.lastModifiedDate or "19700101")) + "_" + (inputs.self.shortRev or "dirty");
         inherit (final) hyprlang;
       };
